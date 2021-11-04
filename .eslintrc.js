9308b0d86e4d2642f9bc4dcb908c163c26d1b65c
Jordi Bonastre <jordi.bonastre.m@gmail.com>
2021-11-04 12:53:34 +0100

    imitial commit with boilerplate

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ["airbnb-base"],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+
+  rules: {
+    "no-param-reassign": "off",
+    "no-plusplus": "off",
+    "no-restricted-syntax": "off",
+    "consistent-return": "off",
+    "no-debugger": "off",
+    "no-console": "off",
+  },
+};
